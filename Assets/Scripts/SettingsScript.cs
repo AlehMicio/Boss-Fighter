7cf714576bd02ad5fa8bb7a94b0565ca1c872c28
AlehMicio <doynickow.oleg@yandex.ru>
2023-02-04 15:18:37 +0300

    1

---
+++ Assets/Scripts/SettingsScript.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SettingsScript : MonoBehaviour
+{
+    private bool isFullScreen = false;
+    public AudioMixer am;
+    private Resolution[] rsl;
+    private List<string> resolutions;
+    public Dropdown dropdown;
+
+    public void Awake()
+    {
+        resolutions = new List<string>();
+        rsl = Screen.resolutions;
+        foreach (var i in rsl)
+        {
+            resolutions.Add(i.width + "x" + i.height);
+        }
+        dropdown.ClearOptions();
+        dropdown.AddOptions(resolutions);
+    }
+    
+    public void FullScreen()
+    {
+        isFullScreen = !isFullScreen;
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void AudioVolume(float sliderValue)
+    {
+        am.SetFloat("masterVolume", sliderValue);
+    }
+
+    public void Quality(int q)
+    {
+        QualitySettings.SetQualityLevel(q);
+    }
+
+    public void Resolution(int r)
+    {
+        Screen.SetResolution(rsl[r].width, rsl[r].height, isFullScreen);        
+    }
+}
